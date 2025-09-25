69890abea7a46fcde0dde891ce5dd1b879364a0f
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2025-09-25 21:13:38 +0100

    tidy up

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 pie-chart
 =========
-README text here.
+
+
+I should really do docs but its really just 
+
+```
+  (pie-chart-pict 110 '(#("Eggs" 1.5) #(Bacon 2.5) #(Pancakes 3.5)) #:colours '("orange" "pink" "green"))
+  (pie-chart-pict 110 '(#(Eggs 7.5) #(Bacon 2.5) #(Pancakes 3.5) #("Beans" 2.5) #(Sausages 0.5)) #:colours '("red" "green" "blue" "pink" "orange"))
+
+```
+
+no legend you have to roll your own
+
--- main.rkt
+++ main.rkt
@@ -2,32 +2,8 @@
 (require pict racket/class racket/draw racket/gui
          (only-in racket/math pi) racket/list/grouping)
 (provide ring-sector standard-ring-sector π pie-chart-pict get-ranges)
-
 (module+ test
   (require rackunit))
-
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included LICENSE-MIT and LICENSE-APACHE files.
-;; If you would prefer to use a different license, replace those files with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
-
-;; Code here
-
 (define (π radians)  (* pi radians))
 
 ;; ring-sector: x y start end inner outer dc
@@ -47,7 +23,7 @@
 ;; draws a ring sector whose top-left corner is at x,y
 ;; start end (radians) inner outer (radius)
 (define (standard-ring-sector x y start end outer inner dc)
-(define (standard-rotation Θ) (+ (- Θ) (π 1/2)))
+  (define (standard-rotation Θ) (+ (- Θ) (π 1/2)))
   (define standard-start (standard-rotation end))
   (define standard-end (standard-rotation start))
   (define diameter (* 2 outer))
@@ -58,7 +34,6 @@
   (send path arc inset inset inner-diameter inner-diameter standard-end standard-start #false) ;; counter-clockwise
   (send path close)
   (send dc draw-path path x y))
-
 
 ;; convert segment values into intervals
 (define (values->cardinals ltb-values)
@@ -74,7 +49,6 @@
   (define datavradians (map (λ (v) (* (π 2) (/ v sum))) datavalues))
   (cardinals->cardinalpairs (values->cardinals datavradians)))
 
-
 ;; pie-chart : (list-of vectors#(name value))  #:colors (listof colours)
 ;; sectors angles are proportional to value , there must be a color for each value
 #; (pie-chart dc '(#(Eggs 1.5) #(Bacon 2.5) #(Pancakes 3.5)) #:colors '("red" "pink" "green" "orange" "blue"))
@@ -82,27 +56,22 @@
 ;; while pie-charts are traditionally ordered from largest to smallest that is left to the source data.
 ;; vs : values
 (define (pie-chart dc diameter vs #:colours colours)
-  (displayln (get-ranges vs))
+  ;(displayln (get-ranges vs))
   (for/list ([c colours]
              [sweep (get-ranges vs)])
     (send dc set-brush (new brush% [color c]))
     (standard-ring-sector 3 3 (first sweep) (second sweep) (/ diameter 2) 0 dc)))
 
 ;; pie-chart-pict
-
 (define (pie-chart-pict diameter data #:colours colours)
   (dc (λ (dc dx dy)
         (let* ([old-brush (send dc get-brush)]
                [old-pen (send dc get-pen)])
           (send dc set-smoothing 'aligned)
           (send dc set-pen (new pen% [width 1] [color "slategray"]))
-          ;; draw the owl
           (pie-chart dc diameter data #:colours colours)
-          (displayln "draw the owl2")
           (send dc set-brush old-brush)
-          (send dc set-pen old-pen))
-
-        ) diameter diameter))
+          (send dc set-pen old-pen))) diameter diameter))
 
 (module+ test
   ;; Is run when using DrRacket or with `raco test`.
@@ -111,45 +80,9 @@
 
 (module+ main
   ;; Is run using DrRacket or the `racket` executable.
-
-  ;; drawing
-  (let* ([target3 (make-bitmap 400 400  #:backing-scale 2)]
-         [tdc3 (new bitmap-dc% [bitmap target3])]
-         [old-brush (send tdc3 get-brush)]
-         [old-pen (send tdc3 get-pen)])
-    (send tdc3 set-smoothing 'aligned)
-    (send tdc3 set-pen (new pen% [width 1] [color "slategray"]))
-    ;; draw the owl
-    (pie-chart tdc3 300 '(#(Eggs 1.5) #(Bacon 2.5) #(Pancakes 3.5)) #:colours '("red" "pink" "green"))
-    
-    (send tdc3 set-brush old-brush)
-    (send tdc3 set-pen old-pen)
-    (make-object image-snip% target3))
-
-  (pie-chart-pict 410 '(#(Eggs 1.5) #(Bacon 2.5) #(Pancakes 3.5)) #:colours '("orange" "pink" "green"))
-  (pie-chart-pict 210 '(#(Eggs 7.5) #(Bacon 2.5) #(Pancakes 3.5)) #:colours '("red" "green" "blue"))
-  (pie-chart-pict 510 '(#(Eggs 5.5) #(Bacon 4.5) #(Pancakes 3.5)) #:colours '("orange" "pink" "green"))
-        
-  ;; (ring-sector x y start end outer inner dc)
-  (define target2 (make-bitmap 400 400  #:backing-scale 2))
-  (define tdc2 (new bitmap-dc% [bitmap target2]))
-  (send tdc2 set-smoothing 'aligned)
+  (pie-chart-pict 110 '(#("Eggs" 1.5) #(Bacon 2.5) #(Pancakes 3.5)) #:colours '("orange" "pink" "green"))
+  (pie-chart-pict 110 '(#(Eggs 7.5) #(Bacon 2.5) #(Pancakes 3.5) #("Beans" 2.5) #(Sausages 0.5)) #:colours '("red" "green" "blue" "pink" "orange"))
   
-  (send tdc2 set-pen (new pen% [width 1] [color "slategray"]))
-  (send tdc2 set-brush (new brush% [color "red"]))
-  (ring-sector 3 3 (π 0/6) (π 7/6) 150 100 tdc2)
-        
-  (send tdc2 set-brush (new brush% [color "blue"]))
-  (ring-sector 3 3 (π 7/6) (π 9/6) 140 100 tdc2)
-
-          
-  (send tdc2 set-brush (new brush% [color "yellow"]))
-  (ring-sector 3 3 (π 11/6) (π 1/2) 140 10 tdc2)
-        
-  (send tdc2 set-brush (new brush% [color "green"]))
-  (ring-sector 3 3 (π 3/6) (π 12/6) 150 160 tdc2)
-
-  (make-object image-snip% target2)
   
   ;; polar-area-diagram  TODO
   ;; sectors divide the disk/ring evenly 
