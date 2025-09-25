3a820b063d3db851cef32d9a03d51e770c43c44c
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2025-09-25 21:27:02 +0100

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
 (define collection "pie-chart")
-(define deps '("base"))
+(define deps '("base" "draw-lib" "gui-lib" "pict"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/pie-chart.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "Draw a pie chart as a pict")
 (define version "0.0")
 (define pkg-authors '(stephendegabrielle))
 (define license '(Apache-2.0 OR MIT))
